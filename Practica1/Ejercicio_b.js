bd27701c2829a81a0408a5efdcf95014733e5a7e
Gabriel <gabriel.mzn.r.xx66@gmail.com>
2025-05-19 15:44:54 -0600

    Commit - Ejercicio b

---
+++ Practica1/Ejercicio_b.js
@@ -0,0 +1,10 @@
+function cuadrado(numero) {
+    return numero * numero;
+}
+
+//console.log();
+//Muestra en consola probando con 3 números diferentes
+
+let cuadrado2 = numero => numero * numero;
+
+console.log(cuadrado2(5));
