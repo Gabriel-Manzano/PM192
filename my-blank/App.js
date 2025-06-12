497dbb911d71c5d871e93d0b3447c2fd699ec69d
Gabriel <gabriel.mzn.r.xx66@gmail.com>
2025-06-11 18:19:29 -0600

    Commit 1 - Práctica 5
    Texto dinámico

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,14 +1,34 @@
+//Zona 1 Importaciones
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const Texto=(props)=>{
+  const {contenido}=props
+  return(
+    <Text> {contenido} </Text>
+  )
+}
+
+//Zona 2: Main
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      
+      <Texto contenido="Come"></Texto>
+      <Texto contenido="Tierra"></Texto>
+      <Texto contenido="Polo"></Texto>
+      <Button title="Button.jpg"></Button>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
+
+//Zona 3: Estilos
 
 const styles = StyleSheet.create({
   container: {
